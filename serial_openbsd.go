ea34c8b530722a5d0d1c5765baea5063bd8d258d
niklas <niklas@appli.se>
2022-11-12 09:35:13 +0100

    On OpenBSD baud rates are not required to be part of the classical set

--- serial_openbsd.go
+++ serial_openbsd.go
@@ -13,30 +13,6 @@ const regexFilter = "^(cu|tty)\\..*"
 
 // termios manipulation functions
 
-var baudrateMap = map[int]uint32{
-	0:      unix.B9600, // Default to 9600
-	50:     unix.B50,
-	75:     unix.B75,
-	110:    unix.B110,
-	134:    unix.B134,
-	150:    unix.B150,
-	200:    unix.B200,
-	300:    unix.B300,
-	600:    unix.B600,
-	1200:   unix.B1200,
-	1800:   unix.B1800,
-	2400:   unix.B2400,
-	4800:   unix.B4800,
-	9600:   unix.B9600,
-	19200:  unix.B19200,
-	38400:  unix.B38400,
-	57600:  unix.B57600,
-	115200: unix.B115200,
-	230400: unix.B230400,
-	//460800: unix.B460800,
-	//921600: unix.B921600,
-}
-
 var databitsMap = map[int]uint32{
 	0: unix.CS8, // Default to 8 bits
 	5: unix.CS5,
@@ -57,18 +33,16 @@ const ioctlTcgetattr = unix.TIOCGETA
 const ioctlTcsetattr = unix.TIOCSETA
 const ioctlTcflsh = unix.TIOCFLUSH
 
-func toTermiosSpeedType(speed uint32) int32 {
+func toTermiosSpeedType(speed int) int32 {
 	return int32(speed)
 }
 
 func setTermSettingsBaudrate(speed int, settings *unix.Termios) (error, bool) {
-	baudrate, ok := baudrateMap[speed]
-	if !ok {
-		return nil, true
+	if speed == 0 {
+		speed = 9600
 	}
-
-	settings.Ispeed = toTermiosSpeedType(baudrate)
-	settings.Ospeed = toTermiosSpeedType(baudrate)
+	settings.Ispeed = toTermiosSpeedType(speed)
+	settings.Ospeed = toTermiosSpeedType(speed)
 	return nil, false
 }
 
